8545cff2ce96f8f561e0e2f3953bac4d4c529d2d
Shubham Goel <sshubhamgoel24@gmail.com>
2021-01-09 18:47:03 +0530

    Added roll a dice

---
+++ Roll-A-Dice/file.js
@@ -0,0 +1,17 @@
+function intr(){
+    z=(Math.floor(Math.random() * Math.floor(7)));
+    if(z==0){
+        z=6;
+    }
+    y=document.getElementById('si').innerHTML=z;
+    x=document.getElementsByClassName('cube')[0];
+    x.classList.remove('leftr');
+}
+
+function roll(){
+    y=document.getElementById('si').innerHTML="";
+    x=document.getElementsByClassName('cube')[0];
+    x.classList.add('leftr');
+    setTimeout(intr,1000)
+}
+
