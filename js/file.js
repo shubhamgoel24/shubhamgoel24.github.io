7a0d8572254767e36b4c7bed4956206729c7da66
Shubham Goel <sshubhamgoel24@gmail.com>
2021-05-18 20:24:24 +0530

    Update file.js

--- js/file.js
+++ js/file.js
@@ -1,6 +1,5 @@
 function openmenu(){
     var x=document.getElementById("menubar");
-    navigator.vibrate(2000);
     if(x.className == "menuclose")
         x.className="menuopen";
     else if(x.className == "menuopen")
