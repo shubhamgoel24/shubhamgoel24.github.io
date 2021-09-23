4d8571be7b5bea7b60a820ec9817902c5d6c07fd
Shubham Goel <sshubhamgoel24@gmail.com>
2021-09-23 11:42:19 +0530

    Responsive Navbar

---
+++ NavBar/script.js
@@ -0,0 +1,9 @@
+navToggle = function(){
+    var x = document.getElementById("navRight");
+    if(x.style.display == "block"){
+        x.style.display = "none";
+    }
+    else{
+        x.style.display = "block";
+    }
+}
